530e31deb715f7edff8dd6e00c02c397fd5f1d6b
hdd <lorenzo.angeli@gmail.com>
2013-04-07 19:52:07 +0100

    rename and add file ability

--- manager.py
+++ manager.py
@@ -7,19 +7,30 @@ from pprint import pformat
 
 
 class StructureManager(object):
+	'''Structure manager class,
+	Provide standard methods to create virtual file structure
+	from disk fragment
 
+	'''
 	def __init__(self, template_folder):
 		self._register = {}
 		self._template_folder = template_folder
 		self.parse()
 
 	def resolve(self, name):
+		'''Return the resolved path of the given variable *name*'''
 		clean_name = name.replace('@', '')
 		entry = {clean_name: {}}
-		self._resolve(self._register.get(name, {}), entry[clean_name])
+
+		self._resolve(
+			self._register.get(name, {}),
+			entry[clean_name]
+		)
+
 		return entry
 
 	def _resolve(self, input, output):
+		'''Recursively resolve the given *input* path into *output*'''
 		for key, value in input.items():
 			if isinstance(value, dict):
 
@@ -31,8 +42,11 @@ class StructureManager(object):
 				else:
 					output.setdefault(key, {})
 					self._resolve(value, output[key])
+			else:
+				output[key] = None
 
 	def parse(self):
+		'''Parse the given template path and fill up the register table'''
 		template_path = os.path.realpath(self._template_folder)
 		templates = os.listdir(template_path)
 
@@ -52,14 +66,23 @@ class StructureManager(object):
 		if os.path.exists(basefolder) and os.path.isdir(basefolder):
 			folders = os.listdir(basefolder)
 			for folder in folders:
-				mapped.setdefault(folder, {})
-				newfolder = os.path.join(basefolder, folder)
-				self._folder_map(newfolder, mapped[folder])
+				subfolder = os.path.join(basefolder, folder)
+				default = {}
+				if os.path.isfile(subfolder):
+					default= None
+				#stats = os.stat(subfolder)
+				#print stats
+				mapped.setdefault(folder, default)
+				self._folder_map(subfolder, mapped[folder])
 
 
 if __name__ == '__main__':
 	M = StructureManager('./templates')
+
 	show_result = M.resolve('@+show+@')
-	seq_result = M.resolve('@+sequence+@')
-	shot_result = M.resolve('@+shot+@')
-	nuke_result = M.resolve('@nuke@')
+	# seq_result = M.resolve('@+sequence+@')
+	# shot_result = M.resolve('@+shot+@')
+	# nuke_result = M.resolve('@nuke@')
+
+	print pformat(show_result)
+	#print pformat(M._register)
