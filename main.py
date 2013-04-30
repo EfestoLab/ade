4d6b25b23bf39807da39c69e248e552689e602d3
hdd <lorenzo.angeli@gmail.com>
2013-04-30 22:31:46 +0100

    use new helper functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import re
 import argparse
 from manager import StructureManager
 
@@ -7,13 +7,11 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument
 	'''Function entry point'''
-	M = StructureManager('./templates')
-	#print 'register: ', pformat(M.register)
-	built = M.resolve_schema('@+show+@')
-	#print 'built: ', pformat(built)
-	results = M.resolve(built)
 
-	pareser_results = M.to_parser(results)
+	M = StructureManager('./templates')
+	schema = '@+show+@'
+
+	pareser_results = M.to_parser(schema)
 	print 'PARSERs:'
 	for result in pareser_results:
 		print '/'.join(result)
@@ -27,7 +25,19 @@ if __name__ == '__main__':
 		'python_version': '2.6.4'
 	}
 
-	pareser_results = M.to_path(results, context)
+	path_results = M.to_path(schema, context)
 	print 'PATHs:'
-	for result in pareser_results:
+	for result in path_results:
 		print '/'.join(result)
+
+	print 'TEST:'
+	for result in pareser_results:
+		regex = '/'.join(result)
+		check = re.compile(regex)
+		match = check.match('/'.join(path_results[4]))
+
+		if not match:
+			continue
+
+		print match.groupdict()
+
