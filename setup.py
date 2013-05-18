f94fa3d9642c44ecef289b009327b3520445d33c
hdd <lorenzo.angeli@gmail.com>
2013-05-18 23:48:59 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python                                                                                                                                        
 from setuptools import setup, find_packages
 
-setup(version='0.1.0',
-      description='Ade, a templated file system manager',
-      author='Lorenzo Angeli',
-      name='ade',
-      author_email='lorenzo.angeli@gmail.com',
-      packages=find_packages(exclude=["test"]),
-      test_suite="test",    
-      entry_points={
-        'console_scripts': [
-            'run_ade = ade.main:run',
-            ],
-        },                     
+setup(
+	version='0.1.0',
+	description='Ade, a templated file system manager',
+	author='Lorenzo Angeli',
+	name='ade',
+	author_email='lorenzo.angeli@gmail.com',
+	packages=find_packages(exclude=["test"]),
+	test_suite="test",
+	entry_points={
+		'console_scripts': [
+		'run_ade = ade.main:run',
+		]
+	}
 )
