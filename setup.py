8be86804d169662095790da06a5ae9294c553ed7
lorenzo.angeli <lorenzo.angeli@gmail.com>
2014-04-27 17:59:12 +0100

    spaces, add requires.

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-#!/usr/bin/env python                                                                                                                                        
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 
 setup(
-	version='0.1.0',
-	description='Ade, a templated file system manager',
-	author='Lorenzo Angeli',
-	name='ade',
-	author_email='lorenzo.angeli@gmail.com',
-	packages=find_packages(exclude=["test"]),
-	include_package_data=True,
-	test_suite="test",
-	entry_points={
-		'console_scripts': [
-		'ade = ade.main:run',
-		],
-	}
+    version='0.1.0',
+    description='Ade, a templated file system manager',
+    author='Lorenzo Angeli',
+    name='ade',
+    author_email='lorenzo.angeli@gmail.com',
+    packages=find_packages(exclude=["test"]),
+    include_package_data=True,
+    test_suite="test",
+    entry_points={
+        'console_scripts': [
+            'ade = ade.main:run',
+        ],
+    },
+    install_requires=[
+        'argparse',
+        'sphinx',
+    ],
 )
